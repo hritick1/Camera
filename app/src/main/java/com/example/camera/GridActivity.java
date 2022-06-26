54417fc8d08fd13b6a9398e4d49afcf2db774f23
hritick1 <hritick.12016375@lpu.in>
2022-06-26 13:01:46 +0530

    Full ImageView
    Created a new Activity where we can see the expanded view of the photos

--- app/src/main/java/com/example/camera/GridActivity.java
+++ app/src/main/java/com/example/camera/GridActivity.java
@@ -8,6 +8,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageView;
@@ -31,6 +32,15 @@ public class GridActivity extends AppCompatActivity {
         }
         Adapter adapter=new Adapter(getApplicationContext(),list);
         gridView.setAdapter(adapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent1=new Intent(GridActivity.this,ViewActivity.class);
+                intent1.putExtra("position",i);
+                intent1.putParcelableArrayListExtra("bitmap",bList);
+                startActivity(intent1);
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/camera/ViewActivity.java
+++ app/src/main/java/com/example/camera/ViewActivity.java
@@ -2,13 +2,29 @@ package com.example.camera;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ViewActivity extends AppCompatActivity {
-
+TextView textView;
+ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
+        textView=findViewById(R.id.TextView);
+        imageView=findViewById(R.id.ImageView);
+
+        Intent intent=getIntent();
+        int i=intent.getExtras().getInt("position");
+        ArrayList<Bitmap> list= intent.getParcelableArrayListExtra("bitmap");
+        textView.setText("Image-"+(i+1));
+        imageView.setImageDrawable(new BitmapDrawable(getApplicationContext().getResources(),list.get(i)));
     }
 }
